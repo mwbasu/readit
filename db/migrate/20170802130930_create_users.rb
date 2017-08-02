5ca68fd9490a5352201baf9c02aa2478039c3250
mwbasu <mw.basu@gmail.com>
2017-08-02 07:13:36 -0700

    day one last commit

--- db/migrate/20170802130930_create_users.rb
+++ db/migrate/20170802130930_create_users.rb
@@ -1,10 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
-  def change
+  def up
     create_table :users do |t|
     	t.string "email", :limit => 50, :null => false
     	t.column "password", :string,:limit => 30, :null => false
-    	t.datetime
       	t.timestamps null: false
     end
+  end
+
+  def down
+  	drop_table :users
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170802130930) do
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string   "email",      limit: 50, null: false
+    t.string   "password",   limit: 30, null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+  end
 
 end
